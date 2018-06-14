56dea37bef7a2af922e120dadac5554140b8f0af
Henintsoa Rasoloson <39616446+HenintsoaRasoloson@users.noreply.github.com>
2018-06-14 10:31:37 +0300

    add application and assets files

---
+++ application/views/Homepage.php
@@ -0,0 +1,4 @@
+
+<div class="container">
+	<p>container</p>
+</div>
---
+++ application/views/Layout/header.php
@@ -0,0 +1,30 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>My Personal Stylist</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/materialize.min.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/mystyle.css">
+</head>
+<html>
+	<div id="container">
+	 <div id="logo">
+	 	<span>MY<b>PERSONAL</b>STYLIST</span><br>
+	 	<span class="auteur"><b>BY MARIE</b></span>
+	 	
+	 </div>
+
+	 <div id="menu">
+	 	<ul class="my-menu">
+	 		<li>ACCUEIL</li>
+	 		<li>A PROPOS</li>
+	 		<li>SERVICES</li>
+	 		<li>TEMOIGNAGES</li>
+	 		<li>CONTACT</li>
+	 		<li></li>
+	 	</ul>
+	 	
+	 </div>
+	</div>
+</html>
