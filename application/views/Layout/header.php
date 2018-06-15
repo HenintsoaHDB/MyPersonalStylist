af71fffb4ff5cff63444e52cd77a1eb7d9579763
Henintsoa R <henintsoa.r@hdb-solutions.com>
2018-06-15 15:36:28 +0300

    fix navbar header

--- application/views/Layout/header.php
+++ application/views/Layout/header.php
@@ -6,25 +6,30 @@
 	<title>My Personal Stylist</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/materialize.min.css">
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/mystyle.css">
+	<!-- <link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/css/fontawesome.min.css"> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <html>
 	<div id="container">
 	 <div id="logo">
 	 	<span>MY<b>PERSONAL</b>STYLIST</span><br>
-	 	<span class="auteur"><b>BY MARIE</b></span>
-	 	
+		 <span class="auteur"><b>BY MARIE</b></span>
 	 </div>
-
-	 <div id="menu">
-	 	<ul class="my-menu">
-	 		<li>ACCUEIL</li>
-	 		<li>A PROPOS</li>
-	 		<li>SERVICES</li>
-	 		<li>TEMOIGNAGES</li>
-	 		<li>CONTACT</li>
-	 		<li></li>
-	 	</ul>
-	 	
-	 </div>
+	 <nav>
+		<div class="nav-wrapper white">
+			<ul id="nav-mobile" class="right hide-on-med-and-down">		
+				<li><a class="black-text darken-4" href="sass.html">ACCUEIL</a></li>
+				<li><a class="black-text darken-4" href="badges.html">A PROPOS</a></li>
+				<li><a class="black-text darken-4" href="collapsible.html">SERVICES</a></li>
+				<li><a class="black-text darken-4" href="collapsible.html">TEMOIGNAGES</a></li>
+				<li><a class="black-text darken-4" href="collapsible.html">CONTACT</a></li>
+				<li class="social-bar">
+					<a class="black-text darken-4 fa fa-facebook-square" href="sass.html"></a>
+					<a class="black-text darken-4 fa fa-instagram" href="sass.html"></a>
+					<a class="black-text darken-4 fa fa-pinterest" href="sass.html"></a>
+				</li>
+			</ul>
+		</div>
+ 	 </nav>
 	</div>
 </html>
