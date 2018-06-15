5cb0235c340e5d6979134bdfce59f83fc3c26391
Henintsoa R <henintsoa.r@hdb-solutions.com>
2018-06-15 15:38:32 +0300

    customizing homepage

--- application/views/Homepage.php
+++ application/views/Homepage.php
@@ -1,4 +1,12 @@
 
+<br><br>
 <div class="container">
-	<p>container</p>
+	<div class="slide_img">
+		<img src ='<?php echo base_url(); ?>assets/img/image2.jpg' style="width:75%;height:75%;margin:auto"></img>
+	</div>
+	<br>
+	<div class="title">
+		<h2>Trouver son propre style</h2>
+	</div>
+
 </div>
